a05ff8a600791ae9cdabc9147c141a1109e68382
Soapistic <housetuto@gmail.com>
2025-05-06 13:22:49 +0100

    changed meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Shadcn/ui Extra',
+  description: 'A collection of custom components built on top of shadcn/ui',
   generator: 'v0.dev',
 }
 
